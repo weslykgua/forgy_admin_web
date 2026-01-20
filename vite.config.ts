309789415d742d994db0dffd75722a18a61c1dc7
Wesly Cagua <weslykgua@gmail.com>
2026-01-20 19:56:00 -0300

    feat: enhance development experience with custom server logging and update package versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type ViteDevServer } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+const prettyLog = () => {
+  return {
+    name: 'custom-print',
+    configureServer(server: ViteDevServer) {
+      server.printUrls = () => {
+        console.log('\n')
+        console.log('  ╔═══════════════════════════════════════════╗')
+        console.log('  ║    💻 FORGY ADMIN PANEL                   ║')
+        console.log(`  ║    ➜ Local:   http://localhost:${server.config.server.port}        ║`)
+        console.log('  ║    ➜ Status:  Online                      ║')
+        console.log('  ╚═══════════════════════════════════════════╝')
+        console.log('\n')
+      }
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), prettyLog()],
+  clearScreen: false,
+  server: {
+    port: 5174,
+    host: true,
+    strictPort: true
+  }
 })
